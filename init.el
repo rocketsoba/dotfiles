8a27bade2bb5be8724feb0aa2f2aac9f0f2a9933
rockyakisoba <mmtrokuroku96@gmail.com>
2016-10-28 20:37:17 +0900

    found easy mistake about flycheck and fixed

--- init.el
+++ init.el
@@ -160,12 +160,9 @@
 ;;----flyheck----
 ;;----まだあまりわかってない----
 ;; (package-install 'flycheck)
-;; (global-flycheck-mode)
-
-;; (require 'flycheck)
- (global-flycheck-mode)
-
-(flycheck-define-checker php2
+(global-flycheck-mode)
+;; (add-hooautocok 'after-init-hook #'global-flycheck-mode)
+(flycheck-define-checker web-mode-php
   "A PHP syntax checker using the PHP command line interpreter.
 
 See URL `http://php.net/manual/en/features.commandline.php'."
@@ -173,30 +170,16 @@ See URL `http://php.net/manual/en/features.commandline.php'."
             "-d" "log_errors=0" source)
   :error-patterns
   ((error line-start (or "Parse" "Fatal" "syntax") " error" (any ":" ",") " "
-          (message) " in " (file-name) " on line " line line-end))
-  :modes (web-mode))
+	  (message) " in " (file-name) " on line " line line-end))
+  :modes (web-mode)
+  )
+
 (add-hook 'web-mode-hook
-          '(lambda()
-             (flycheck-select-checker 'php2)))
-;; (add-hooautocok 'after-init-hook #'global-flycheck-mode)
-;; (flycheck-define-checker web-mode-php
-;;   "A PHP syntax checker using the PHP command line interpreter.
-
-;; See URL `http://php.net/manual/en/features.commandline.php'."
-;;   :command ("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1"
-;;             "-d" "log_errors=0")
-;;   :error-patterns
-;;   ((error line-start (or "Parse" "Fatal" "syntax") " error" (any ":" ",") " "
-;;           (message) " in - on line " line line-end))
-;;   :modes (web-mode)
-;;   )
-
-;; (add-hook 'web-mode-hook
-;;           (lambda ()
-;;             (when (equal web-mode-engine "php")
-;;               ;; enable flycheck
-;;               (flycheck-select-checker 'web-mode-php)
-;;               (flycheck-mode))))
+          (lambda ()
+            (when (equal web-mode-engine "php")
+              ;; enable flycheck
+              (flycheck-select-checker 'web-mode-php)
+              (flycheck-mode))))
 
 ;;----ファイル重複時にDIR表示----
 (require 'uniquify)
