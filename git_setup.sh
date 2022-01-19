e1a8ebd558c6471cdc83ca654503c2041d02c4aa
rockyakisoba <mmtrokuroku96@gmail.com>
2022-01-19 16:35:54 +0900

    gitの設定

---
+++ git_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if command -v git > /dev/null 2>&1; then
+    if ! git --no-pager  config --get-all url."git@github.com:".pushInsteadOf > /dev/null 2>&1; then
+        git config --global       url."git@github.com:".pushInsteadOf "git://github.com/"
+        git config --global --add url."git@github.com:".pushInsteadOf "https://github.com/"
+    fi
+    git config --global       url."git://github.com/".insteadOf "https://github.com/"
+    git config --global       diff.algorithm histogram
+    git config --global       alias.tree 'log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
+    git config --global       alias.st 'status -sb'
+    git config --global       core.pager 'less -+FX -M'
+fi
