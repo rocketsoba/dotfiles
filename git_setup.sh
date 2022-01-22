8c342aba608895da05cc898cc42ac99a4f7df83a
rockyakisoba <mmtrokuroku96@gmail.com>
2022-01-23 02:47:51 +0900

    .gitconfigの追加設定

--- git_setup.sh
+++ git_setup.sh
@@ -6,6 +6,9 @@ if command -v git > /dev/null 2>&1; then
         git config --global --add url."git@github.com:".pushInsteadOf "https://github.com/"
     fi
     git config --global       url."git://github.com/".insteadOf "https://github.com/"
+    git config --global       merge.ff false
+    git config --global       pull.ff only
+    git config --global       push.default current
     git config --global       diff.algorithm histogram
     git config --global       alias.tree 'log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
     git config --global       alias.st 'status -sb'
