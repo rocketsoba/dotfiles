148335322434168fe3422fc7b7c64a92fb54b24e
rockyakisoba <mmtrokuroku96@gmail.com>
2022-03-05 19:46:49 +0900

    phpctagsの互換性問題の対処

--- php_setup.sh
+++ php_setup.sh
@@ -26,6 +26,26 @@ php_config_print() {
     echo "# ----------------------------------------------------------------------"
 }
 
+inject_emacs_phpctags() {
+    case $1 in
+        php56)
+            local PHPCTAGS_URL="https://github.com/xcwen/ac-php/raw/362907ca3dac0b5525a6881678e0f07b82f7a77f/phpctags"
+            ;;
+        *)
+            local PHPCTAGS_URL="https://github.com/xcwen/ac-php/raw/master/phpctags"
+            ;;
+    esac
+    local AC_PHP_CORE_PATH=$(find ~/.emacs.d/ -name 'ac-php-core*' -type d | head -n1)
+    if [ -d "$AC_PHP_CORE_PATH" ]; then
+        if [ -f ${AC_PHP_CORE_PATH}/phpctags ]; then
+            rm -f ${AC_PHP_CORE_PATH}/phpctags
+        fi
+        curl -Lso ${AC_PHP_CORE_PATH}/phpctags $PHPCTAGS_URL
+        chmod +x ${AC_PHP_CORE_PATH}/phpctags
+        rm -rf ~/.ac-php/
+    fi
+}
+
 php_config_write() {
     cat ${HOME}"/.bash_profile" > ${HOME}"/.bash_profile.bak"
     if grep "# ----Automatically generated php config by rocketsoba/build-scripts----" ${HOME}"/.bash_profile.bak" 2>&1 > /dev/null; then
@@ -51,14 +71,18 @@ php_config_write() {
 case "$1" in
     php56)
         php_config_write "php56"
+        inject_emacs_phpctags "php56"
         ;;
     php74)
         php_config_write "php74"
+        inject_emacs_phpctags "php74"
         ;;
     php81)
         php_config_write "php81"
+        inject_emacs_phpctags "php81"
         ;;
     *)
         php_config_write "php56"
+        inject_emacs_phpctags "php56"
         ;;
 esac
