f6e36f6e8240135d4e0c7c131f0119906f190a58
rockyakisoba <mmtrokuroku96@gmail.com>
2022-04-21 15:05:05 +0900

    不要コードを削除、リファクタリング

--- init.el
+++ init.el
@@ -5,50 +5,8 @@
 ;;----"M-x transient-mark-mode"で選択範囲の色を出すか切り替える----
 ;;; Code:
 (column-number-mode t)
-;; (global-linum-mode t)
-;;----"M-x linum-mode"で切り替え----x
 ;; (display-time)
 
-
-
-;; Mac環境でnw,GUIともになぜか動作しない
-(cond
- ((equal system-type 'darwin)
-  (cond
-   ((window-system)
-
-    (set-face-attribute 'default nil
-                        :family "monaco" ;; font
-                        :height 150)     ;; font size
-    (set-fontset-font
-     (frame-parameter nil 'font)
-     'japanese-jisx0208
-     '("Hiragino Maru Gothic ProN" . "iso10646-1"))
-    (setq initial-frame-alist
-          '((width . 80) (height . 40)))
-    ))
-  ))
-(cond
- ((equal system-type 'usg-unix-v)
-  ;; (cond
-  ;;  ((featurep 'x-toolkit)
-  ;; (custom-set-faces '(default ((t (:family "unknown-DejaVu Sans" :height 13 :weight normal)))))
-  (add-to-list 'default-frame-alist
-               '(font . "helvetica:size=13:weight=bold"))
-  ;; (create-fontset-from-ascii-font "DejaVu Sans Mono-12:weight=normal" nil "Dejavu")
-  ;; (set-face-attribute 'default nil
-  ;;       :family "DejaVu Sans Mono" :height 140)
-  ;; (set-fontset-font  (frame-parameter nil 'font)
-  ;;      'japanese-jisx0208
-  ;;      (font-spec :family "DejaVu Sans Mono"))
-  ;; (custom-set-faces '(default ((t (:family "fixed" :foundry "sony" :slant normal :weight normal :height 128 :width normal)))))
-  (setq initial-frame-alist
-        '((width . 100) (height . 50)))
-  ;; (custom-set-variables
-  ;;  '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
-  ;; '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
-  ))
-;; (setq inhibit-startup-message t)
 (menu-bar-mode 0)
 ;; スクロールマウスの設定
 (global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 5)))
@@ -67,47 +25,31 @@
 (setq backup-directory-alist `((".*" . "~/.emacs.d/backup/")))
 (setq auto-save-file-name-transforms `((".*", "~/.emacs.d/backup/" t)))
 
-;; これはtramp時のバックアップを無効にするのではなくバックアップ先ディレクトリをカレントディレクトリにするだけ
-;; (add-to-list 'backup-directory-alist
-;;                                (cons tramp-file-name-regexp nil))
-
-
 ;;----MELPAレポジトリ追加(emacs24以降)----
 (require 'package)
 (setq package-archives `(("melpa" . "http://melpa.org/packages/")))
-;; (setq package-archives `(("melpa" . "http://stable.melpa.org/packages/")))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-;; (add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
-;; (package-refresh-contents)
-;;----(if (not (require 'hoge nil t)) (任意))はmustっぽい----
-;; (if (not (require 'flycheck nil t))
-;;     (package-refresh-contents)
-;;   )
-(defvar initflag 0)
+
+(defvar my-initflag 0)
 (defvar my-package '(zenburn-theme verilog-mode web-mode auto-complete flycheck go-mode badwolf-theme basic-theme nyan-mode flycheck-pos-tip c-eldoc ac-php vimrc-mode nlinum nlinum-relative undo-tree anzu sql-indent geben multiple-cursors json-mode planet-theme indent-guide smooth-scrolling helm helm-gtags yaml-mode wgrep))
 (dolist (package my-package)
   (unless (package-installed-p package)
     (progn
-      (if (equal initflag 0)
+      (if (equal my-initflag 0)
           (package-refresh-contents)
         )
-      (setq initflag 1)
+      (setq my-initflag 1)
       (package-install package)
       )
     )
   )
-
-(setq desktop-globals-to-save '(extended-command-history))
-(setq desktop-files-not-to-save "")
-(desktop-save-mode 1)
 
 ;; (require 'helm-config)
 ;; (require 'helm-gtags)
 ;; (helm-mode 1)
 ;; (global-set-key (kbd "M-x") 'helm-M-x)
 
-;; (require 'nyan-mode)
 (setq nyan-bar-length 16)
 (nyan-mode)
 (nyan-start-animation)
@@ -181,7 +123,6 @@ See Info Node `(elisp)Byte Compilation'."
 ;;----web-mode----
 ;;----"http://yanmoo.blogspot.jp/2013/06/html5web-mode.html"----
 (add-to-list 'auto-mode-alist '("\\.\\([xps]html\\|html\\|tpl\\|php\\|js\\|ctp\\)\\'" . web-mode))
-;; (add-to-list 'web-mode-content-types' ("php" . "\\.php\\'"))
 (setq-default indent-tabs-mode nil)
 (autoload 'web-mode "web-mode" nil t)
 (defun my-web-mode-hook ()
@@ -339,56 +280,36 @@ See Info Node `(elisp)Byte Compilation'."
 
       (when pos-after (goto-char pos-after))
       ))
-  ;; (require 'ac-php)
-  ;; (setq ac-sources  '(ac-source-php ) )
-  ;; (yas-global-mode 1)
-  ;; (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
-  ;; (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+
   (hs-minor-mode 1)
   (when (equal web-mode-engine "php")
-    ;; enable flycheck
     (setq flycheck-phpcs-standard "PSR12")
-    ;; (flycheck-add-next-checker 'web-mode-php 'web-mode-php-phpcs)
-    ;; (flycheck-select-checker 'web-mode-php)
     (flycheck-add-next-checker 'php 'php-phpcs)
     (flycheck-add-mode 'php 'web-mode)
     (flycheck-add-mode 'php-phpcs 'web-mode)
+
+    ;; https://github.com/xcwen/ac-php
     (auto-complete-mode t)
     (require 'ac-php)
     (setq ac-sources  '(ac-source-php ) )
     (yas-global-mode 1)
-    (ac-php-core-eldoc-setup ) ;; enable eldoc
-    ;; (flycheck-mode)
+    (ac-php-core-eldoc-setup)
+    )
+  (when (equal (file-name-extension buffer-file-name) "ctp")
+    (add-to-list 'flycheck-disabled-checkers 'php-phpcs)
     )
   (when (equal web-mode-content-type "javascript")
-    ;; enable flycheck
     (add-to-list 'flycheck-disabled-checkers 'php)
     (add-to-list 'flycheck-disabled-checkers 'php-phpcs)
     (add-to-list 'flycheck-disabled-checkers 'javascript-jshint)
     (add-to-list 'flycheck-disabled-checkers 'javascript-jscs)
     (flycheck-add-mode 'javascript-eslint 'web-mode)
-    ;; (flycheck-mode)
-    )
-  (when (equal (file-name-extension buffer-file-name) "ctp")
-    ;; enable flycheck
-    (add-to-list 'flycheck-disabled-checkers 'php-phpcs)
-    ;; (flycheck-mode)
-    )
-  (when (and (not (equal web-mode-engine "php")) (not (equal web-mode-content-type "javascript")))
-    (add-to-list 'flycheck-disabled-checkers 'php)
-    (add-to-list 'flycheck-disabled-checkers 'php-phpcs)
-    ;; (add-to-list 'flycheck-disabled-checkers 'web-mode-php)
-    ;; (add-to-list 'flycheck-disabled-checkers 'web-mode-php-phpcs)
     )
   )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
-;; (eval-after-load "web-mode"
-;;   '(add-to-list 'web-mode-content-types' ("php" . "\\.php\\'")))
 
-;;----themeをzenburnに----
-;;----他のtheme----
+;;----theme----
 ;;----"https://emacsthemes.com/"----
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (cond
  ((window-system)
   (load-theme 'monokai t)
@@ -399,102 +320,17 @@ See Info Node `(elisp)Byte Compilation'."
   )
  )
 
-
 ;;----auto-complete.el----
 ;;----"http://fukuyama.co/emacs-auto-complete"----
 ;;----別ファイルなしでもdict読み込めるかもしれない----
 ;; (require 'auto-complete)
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/lisp/dict")
-;; (defvar web-mode-ac-sources-alist
-;;   '(("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
-;;     ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
 (ac-config-default)
 
-;; (add-hook 'web-mode-before-auto-complete-hooks
-;;           '(lambda ()
-;;              (let ((web-mode-cur-language
-;;                     (web-mode-language-at-pos)))
-;;                (if (string= web-mode-cur-language "php")
-;;                    (yas-activate-extra-mode 'php-mode)
-;;                  (yas-deactivate-extra-mode 'php-mode))
-;;                (if (string= web-mode-cur-language "css")
-;;                    (setq emmet-use-css-transform t)
-;;                  (setq emmet-use-css-transform nil)))))
-;; (add-to-list 'ac-modes 'web-mode)
-
 ;;----flyheck----
-;;----まだあまりわかってない----
-;; (package-install 'flycheck)
 (global-flycheck-mode)
-;; (add-hooautocok 'after-init-hook #'global-flycheck-mode)
-
-
-(flycheck-define-checker web-mode-php-phpcs
-  "A PHP style checker using PHP Code Sniffer.
-
-Needs PHP Code Sniffer 2.6 or newer.
-
-See URL `http://pear.php.net/package/PHP_CodeSniffer/'."
-  :command ("phpcs" "--report=checkstyle"
-            (option "--standard=" flycheck-phpcs-standard concat)
-            ;; Pass original file name to phpcs.  We need to concat explicitly
-            ;; here, because phpcs really insists to get option and argument as
-            ;; a single command line argument :|
-            (eval (when (buffer-file-name)
-                    (concat "--stdin-path=" (buffer-file-name)))))
-  :standard-input t
-  :error-parser flycheck-parse-checkstyle
-  :error-filter
-  (lambda (errors)
-    (flycheck-sanitize-errors
-     (flycheck-remove-error-file-names "STDIN" errors)))
-  :modes (web-mode)
-  ;; phpcs seems to choke on empty standard input, hence skip phpcs if the
-  ;; buffer is empty, see https://github.com/flycheck/flycheck/issues/907
-  :predicate (lambda () (not (flycheck-buffer-empty-p)))
-
-  )
-;; (add-to-list 'flycheck-checkers 'web-mode-php-phpcs)
-
-(flycheck-def-option-var flycheck-web-mode-phpmd-rulesets
-    '("codesize" "design" "naming")
-    php-phpmd
-  "The rule sets for PHP Mess Detector.
-
-Set default rule sets and custom rule set files.
-
-See section \"Using multiple rule sets\" in the PHP Mess Detector
-manual at URL `https://phpmd.org/documentation/index.html'."
-  :type '(repeat :tag "rule sets"
-                 (string :tag "A filename or rule set"))
-  :safe #'flycheck-string-list-p)
-
-(flycheck-define-checker web-mode-php-phpmd
-  "A PHP style checker using PHP Mess Detector.
-
-See URL `https://phpmd.org/'."
-  :command ("phpmd" source "xml"
-            (eval (flycheck-option-comma-separated-list
-                   flycheck-web-mode-phpmd-rulesets)))
-  :error-parser flycheck-parse-phpmd
-  :modes (web-mode))
-
-;; (add-to-list 'flycheck-checkers 'web-mode-php-phpmd)
-
-(flycheck-define-checker web-mode-php
-  "A PHP syntax checker using the PHP command line interpreter.
-
-See URL `http://php.net/manual/en/features.commandline.php'."
-  :command ("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1"
-            "-d" "log_errors=0" source)
-  :error-patterns
-  ((error line-start (or "Parse" "Fatal" "syntax") " error" (any ":" ",") " "
-          (message) " in " (file-name) " on line " line line-end))
-  :modes (web-mode)
-  :next-checkers ((warning . web-mode-php-phpcs))
-  )
-;; (add-to-list 'flycheck-checkers 'web-mode-php)
+(flycheck-pos-tip-mode)
 
 (flycheck-define-checker c/c++-gcc-2
   "A C/C++ syntax checker using GCC.
@@ -548,31 +384,12 @@ Requires GCC 4.8 or newer.  See URL `https://gcc.gnu.org/'."
 
 (add-to-list 'flycheck-checkers 'c/c++-gcc-2)
 
-;; (add-hook 'web-mode-hook
-;;            (lambda ()
-;;              (when (equal web-mode-engine "php")
-;;                ;; enable flycheck
-;;                (setq flycheck-phpcs-standard "PSR2")
-;;                (flycheck-add-next-checker 'web-mode-php 'web-mode-php-phpcs)
-;;                (flycheck-select-checker 'web-mode-php)
-;;                ;; (flycheck-mode)
-;;                )
-;;              (when (not (equal web-mode-engine "php"))
-;;                (add-to-list 'flycheck-disabled-checkers 'web-mode-php)
-;;                (add-to-list 'flycheck-disabled-checkers 'web-mode-php-phpcs)
-;;                )
-;;              )
-;;            )
-
-
 (add-hook 'c-mode-common-hook
           (lambda ()
             (flycheck-select-checker 'c/c++-gcc-2)
             (flycheck-mode)
             (c-turn-on-eldoc-mode)
             ))
-
-(flycheck-pos-tip-mode)
 
 (setq comment-style 'indent)
 
@@ -585,22 +402,7 @@ Requires GCC 4.8 or newer.  See URL `https://gcc.gnu.org/'."
 (setq recentf-max-saved-items 500)
 (recentf-mode +1)
 
-
-;;Load verilog-mode only when needed
-(autoload 'verilog-mode "verilog-mode" nil t )
-
-(add-hook 'java-mode-hook (lambda ()
-                            (setq c-basic-offset 2
-                                  tab-width 2
-                                  indent-tabs-mode t)))
-
-;; Any files that end in .v should be in verilog mode
-(add-to-list 'auto-mode-alist '("\\.\\(verilog\\|template\\)\\'" . verilog-mode))
-
-;; インデントハイライトのせってい
-;; ↓めっちゃバグる
-;; (setq highlight-indent-guides-method 'character)
-;; (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+;; indent
 (require 'indent-guide)
 (indent-guide-global-mode)
 ;; (setq indent-guide-recursive t)
@@ -612,9 +414,7 @@ Requires GCC 4.8 or newer.  See URL `https://gcc.gnu.org/'."
 ;; nlinum
 (global-nlinum-mode t)
 (setq nlinum-format "%3d ")
-;; (require 'nlinum-relative)
-;; (add-hook 'prog-mode-hook 'nlinum-relative-mode)
-;; (setq nlinum-relative-offset 0)
+
 
 (global-anzu-mode +1)
 
@@ -629,13 +429,11 @@ Requires GCC 4.8 or newer.  See URL `https://gcc.gnu.org/'."
 
 ;; whitespace-mode
 (require 'whitespace)
-(setq whitespace-style '(face           ; faceで可視化
-                         trailing       ; 行末
-                         tabs           ; タブ
-                         ;;                         empty          ; 先頭/末尾の空行
-                         space-mark     ; 表示のマッピング
-                         tab-mark
-                         ))
+(setq whitespace-style '(face
+                         trailing
+                         tabs
+                         space-mark
+                         tab-mark))
 (setq whitespace-display-mappings
       '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
 (global-whitespace-mode 1)
@@ -647,105 +445,16 @@ Requires GCC 4.8 or newer.  See URL `https://gcc.gnu.org/'."
 (savehist-mode 1)
 (setq savehist-additional-variables '(extended-command-history))
 
-(cond
- ((package-installed-p "anything")
-
-  (require 'cl)  ; loop, delete-duplicates
-  (defun anything-font-families ()
-    "Preconfigured `anything' for font family."
-    (interactive)
-    (flet ((anything-mp-highlight-match () nil))
-          (anything-other-buffer
-           '(anything-c-source-font-families)
-           "*anything font families*")))
-
-  (defun anything-font-families-create-buffer ()
-    (with-current-buffer
-        (get-buffer-create "*Fonts*")
-      (loop for family in (sort (delete-duplicates (font-family-list)) 'string<)
-            do (insert
-                (propertize (concat family "\n")
-                            'font-lock-face
-                            (list :family family :height 2.0 :weight 'bold))))
-      (font-lock-mode 1)))
-
-  (defvar anything-c-source-font-families
-    '((name . "Fonts")
-      (init lambda ()
-            (unless (anything-candidate-buffer)
-              (save-window-excursion
-                (anything-font-families-create-buffer))
-              (anything-candidate-buffer
-               (get-buffer "*Fonts*"))))
-      (candidates-in-buffer)
-      (get-line . buffer-substring)
-      (action
-       ("Copy Name" lambda
-        (candidate)
-        (kill-new candidate))
-       ("Insert Name" lambda
-        (candidate)
-        (with-current-buffer anything-current-buffer
-          (insert candidate))))))
-  ))
-
-;; Any files in verilog mode shuold have their keywords colorized
-
 ;;----x関連をwithでビルドすると動きそう----
 ;; (require 'simpleclip)
 ;; (xterm-mouse-mode t)
 ;; (simpleclip-mode t)
 
-;;---el-get.el----
-;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-;; (unless (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-;;     (goto-char (point-max))
-;;     (eval-print-last-sexp)))
-;; (require 'el-get)
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/elpa")
-;; (el-get 'sync)
-;;----el-getによってgit,emacswiki,ELPAなどからpackageがとってこれる----
-;;----が、各package設定がよくわからないし管理もわからなかったのでそのうちやる----
-;;----git管理&無意味なディレクトリ構造がなくなってよさそう----
-;;----not without-gnutlsでビルドが必要----
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:inherit nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
-;; (add-to-list 'default-frame-alist '(alpha . (0.90 0.90)))
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:background "unspecified-bg" :slant normal :weight normal :height 1 :width normal :foundry "defau;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
-;; (put 'downcase-region 'disabled nil)
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(custom-safe-themes
-;;    (quote
-;;     ("4e4d9f6e1f5b50805478c5630be80cce40bee4e640077e1a6a7c78490765b03f" "604648621aebec024d47c352b8e3411e63bdb384367c3dd2e8db39df81b475f5" default))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
 (put 'upcase-region 'disabled nil)
 
 ;; wgrep
 (setf wgrep-enable-key "e")
 (setq wgrep-auto-save-buffer t)
+
+(provide 'init)
+;;; init.el ends here
