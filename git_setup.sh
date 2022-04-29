a8abbfd315cd4105f412f02b93443a3c7c404a08
rockyakisoba <mmtrokuroku96@gmail.com>
2022-04-29 12:59:16 +0900

    git://スキームのfetchを削除

--- git_setup.sh
+++ git_setup.sh
@@ -2,15 +2,16 @@
 
 if command -v git > /dev/null 2>&1; then
     if ! git --no-pager  config --get-all url."git@github.com:".pushInsteadOf > /dev/null 2>&1; then
-        git config --global       url."git@github.com:".pushInsteadOf "git://github.com/"
-        git config --global --add url."git@github.com:".pushInsteadOf "https://github.com/"
+        git config --global         url."git@github.com:".pushInsteadOf "git://github.com/"
+        git config --global --add   url."git@github.com:".pushInsteadOf "https://github.com/"
     fi
-    git config --global       url."git://github.com/".insteadOf "https://github.com/"
-    git config --global       merge.ff false
-    git config --global       pull.ff only
-    git config --global       push.default current
-    git config --global       diff.algorithm histogram
-    git config --global       alias.tree 'log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
-    git config --global       alias.st 'status -sb'
-    git config --global       core.pager 'less -+FX -M'
+    git config --global         merge.ff false
+    git config --global         pull.ff only
+    git config --global         push.default current
+    git config --global         diff.algorithm histogram
+    git config --global         alias.tree 'log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
+    git config --global         alias.st 'status -sb'
+    git config --global         core.pager 'less -+FX -M'
+
+    git config --global --unset url."git://github.com/".insteadOf
 fi
