582af093929968f626465a0df7f15bb285c929bf
rockyakisoba <mmtrokuroku96@gmail.com>
2023-11-03 20:46:14 +0900

    bash-completionを追加

--- completion_setup.sh
+++ completion_setup.sh
@@ -7,6 +7,12 @@ fi
 curl -Lso ~/.bash_completion.d/phpunit.completion.bash https://raw.githubusercontent.com/sjorek/phpunit-bash-completion/master/phpunit-completion.bash
 curl -Lso ~/.bash_completion.d/composer.completion.bash https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/composer.completion.bash
 curl -Lso ~/.bash_completion.d/tmux.completion.bash https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash
+curl -Lso ~/.bash_completion.d/nvm.completion.bash https://raw.githubusercontent.com/nvm-sh/nvm/master/bash_completion
+
+if command -v npm > /dev/null 2>&1; then
+    npm completion > ~/.bash_completion.d/npm.completion.bash
+fi
 
 sed -i -e 's/^about-completion.*//g' ~/.bash_completion.d/composer.completion.bash
 sed -i -e 's/^cite.*//g' ~/.bash_completion.d/composer.completion.bash
+sed -i -e "/^if ! command -v nvm/,/fi/d" ~/.bash_completion.d/nvm.completion.bash
